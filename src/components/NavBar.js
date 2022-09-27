15b3c23bfab1d2ae00aa3bbe611fa80dc3fc1c85
Abhinav Kumar Jha <akj0712@outlook.com>
2022-09-27 08:22:02 +0530

    active

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,9 @@ const NavBar = () => {
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">
+          <Link className={`navbar-brand ${
+                  location.pathname === "/about" ? "active" : ""
+                }`} to="/">
             News Anchor
           </Link>
           <button
@@ -22,32 +24,44 @@ const NavBar = () => {
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
+              <li className={`nav-link ${
+                  location.pathname === "/about" ? "active" : ""
+                }`}>
                 <Link to="/business" className="nav-link">
                   Business
                 </Link>
               </li>
-              <li className="nav-item">
+              <li className={`nav-link ${
+                  location.pathname === "/about" ? "active" : ""
+                }`}>
                 <Link to="/entertainment" className="nav-link">
                   Entertainment
                 </Link>
               </li>
-              <li className="nav-item">
+              <li className={`nav-link ${
+                  location.pathname === "/about" ? "active" : ""
+                }`}>
                 <Link to="/health" className="nav-link">
                   Health
                 </Link>
               </li>
-              <li className="nav-item">
+              <li className={`nav-link ${
+                  location.pathname === "/about" ? "active" : ""
+                }`}>
                 <Link to="/science" className="nav-link">
                   Science
                 </Link>
               </li>
-              <li className="nav-item">
+              <li className={`nav-link ${
+                  location.pathname === "/about" ? "active" : ""
+                }`}>
                 <Link to="/sports" className="nav-link">
                   Sports
                 </Link>
               </li>
-              <li className="nav-item">
+              <li className={`nav-link ${
+                  location.pathname === "/about" ? "active" : ""
+                }`}>
                 <Link to="/technology" className="nav-link">
                   Technology
                 </Link>
